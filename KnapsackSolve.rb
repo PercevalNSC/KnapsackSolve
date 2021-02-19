aa9990c08c0dc8ead7619fb613fe060c18a4aa76
PercevalNSC <wait-keita1998@outlook.jp>
2021-02-19 18:13:58 +0900

    add pickL

--- KnapsackSolve.rb
+++ KnapsackSolve.rb
@@ -8,6 +8,7 @@ class KnapackSolve
         @knapsackWeight = knapsackWeight
         @problems = Array.new()
     end
+    # 分枝での変数選択
     def selectval(subproblem)
         for i in 1 .. @itemCosts.length 
             flag = 0
@@ -24,6 +25,7 @@ class KnapackSolve
         puts "no selectable variable"
         return 0
     end
+    # subproblemで既知の変数から目的関数値の差を返す
     def differentFunction(subproblem)
         result = 0
         subproblem.each do |sub|
@@ -35,7 +37,7 @@ end
 
 class ProblemSolve
     attr_reader :xCeiling, :xFloor, :optimalSolutionCeiling, :optimalSolutionFloor
-    def initialize(itemCosts, itemWeights, knapsacWeight)
+    def initialize(itemCosts, itemWeights, knapsackWeight)
         @itemCosts = itemCosts
         @itemWeights = itemWeights
         @knapsackWeight = knapsackWeight
@@ -44,19 +46,36 @@ class ProblemSolve
         @optimalSolutionCeiling = 0
         @optimalSolutionFloor = 0
     end
+    # アイテムの重量の和がナップサックの容量を初めて超える添え字を返す
+    def pickL
+        sum = 0
+        index = 0
+        @itemWeights.each do |item|
+            sum += item
+            index += 1
+            if sum > @knapsackWeight then
+                return index
+            end
+        end
+        return 0
+    end
+
 end
 
 # ---- test ----
 if __FILE__ == $0
     itemCosts = [3, 5, 7, 4, 10]
     itemWeights = [1, 2, 3, 2, 5]
-    knapsackWeight = 9
+    knapsackWeight = 14
 
     kpsolve = KnapackSolve.new(itemCosts, itemWeights, knapsackWeight)
 
     subproblem = [[1, 0], [2, 1]]
     puts kpsolve.selectval(subproblem)
     puts kpsolve.differentFunction(subproblem)
+
+    ps = ProblemSolve.new(itemCosts, itemWeights, knapsackWeight)
+    puts ps.pickL()
 end
 
 
